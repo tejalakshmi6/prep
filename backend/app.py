89538c1a036285b9e0e51bf71d3414ffe95a1cdd
Tejalakshmi <tejalakshmi.sethu@gmail.com>
2026-02-14 17:50:31 +0530

    joke

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "SATWA 2026 Prep Running"}
+    return {"message": "SATWA backend running"}
+
+@app.get("/joke")
+def get_joke():
+    url = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(url)
+    data = response.json()
+
+    return {
+        "setup": data["setup"],
+        "punchline": data["punchline"]
+    }
