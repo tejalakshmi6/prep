7e5693049c555977475f1e5c42bdecdcc303a7b3
Tejalakshmi <tejalakshmi.sethu@gmail.com>
2026-02-14 19:15:34 +0530

    mvp: secure api key and update backend/frontend

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,51 @@
-import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "SATWA backend running"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/joke")
-def get_joke():
-    url = "https://official-joke-api.appspot.com/random_joke"
-    response = requests.get(url)
-    data = response.json()
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HF_API_KEY = os.getenv("HF_API_KEY")
+
+class Notes(BaseModel):
+    text: str
+
+
+@app.post("/summarize")
+def summarize_notes(notes: Notes):
+
+    API_URL = "https://router.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
+
+    headers = {
+        "Authorization": f"Bearer {HF_API_KEY}"
+    }
+
+    payload = {
+        "inputs": notes.text
+    }
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+    result = response.json()
+
+    if response.status_code == 200 and isinstance(result, list):
+        summary = result[0]["summary_text"]
+    else:
+        summary = result.get("error", result)
 
     return {
-        "setup": data["setup"],
-        "punchline": data["punchline"]
+        "status_code": response.status_code,
+        "summary": summary
     }
